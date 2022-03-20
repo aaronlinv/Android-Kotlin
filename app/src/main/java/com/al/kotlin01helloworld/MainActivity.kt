f41cb5dd2c053110456a516f565d2900e3145c1f
AaronLin <1546848781@qq.com>
2022-03-20 21:28:14 +0800

    第三讲：Intent拨打电话

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.al.kotlin01helloworld
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -16,7 +22,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         tvInfo = findViewById(R.id.tvInfo)
-        tvInfo.text = "未选中菜单"
+        val btnCall: Button = findViewById(R.id.btnCall)
+        val etPhone: EditText = findViewById(R.id.etPhone)
+
+        etPhone.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                var len = s.toString().length
+                if (len == 11) {
+                    tvInfo.text = "还需要输 $len 位"
+                } else {
+                    tvInfo.text = "还需要输 ${11 - len} 位"
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+        btnCall.setOnClickListener {
+            val phoneNum = etPhone.text.toString()
+            if (phoneNum.length == 11) {
+                callPhone(phoneNum)
+            } else {
+                showToast(this, "号码格式不正确")
+            }
+        }
+    }
+
+    private fun callPhone(phoneNum: String) {
+        val intent = Intent()
+        intent.action = Intent.ACTION_DIAL
+        // 这个tel不能改，后面的数字可以随便改
+        intent.data = Uri.parse("tel:$phoneNum")
+        startActivity(intent)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
