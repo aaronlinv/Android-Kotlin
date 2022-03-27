85ce7a5fc9718d8c843472899cb425c09f6a038d
AaronLin <1546848781@qq.com>
2022-03-27 15:33:37 +0800

    第四讲：配置改变保存变量

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,15 +1,36 @@
 package com.al.kotlin01helloworld
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // 隐藏 ActionBar
         supportActionBar?.hide()
+        val btnCount: Button = findViewById(R.id.button)
+        val tvCount: TextView = findViewById(R.id.textView)
+        val etCount: EditText = findViewById(R.id.editText)
 
+        btnCount.setOnClickListener {
+            counter++
+            tvCount.text = "计数值：$counter"
+        }
+        if (savedInstanceState != null) {
+            tvCount.text = "计数值：${savedInstanceState.get("counter")}"
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // 保存实例数据
+        outState.putInt("counter", counter)
+        myLog("计数值保存：$counter")
     }
 }
