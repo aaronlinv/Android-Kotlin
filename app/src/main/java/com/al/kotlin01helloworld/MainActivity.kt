e9fe9fef53d0a81d6b7e2dc076a527cf910f26af
AaronLin <1546848781@qq.com>
2022-03-27 15:04:31 +0800

    Revert "第四讲：回调 生命周期"
    This reverts commit c846b781

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,53 +1,15 @@
 package com.al.kotlin01helloworld
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        myLog("onCreate")
 
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // 隐藏 ActionBar
         supportActionBar?.hide()
 
-        val btnGo: Button = findViewById(R.id.button_go)
-        btnGo.setOnClickListener {
-            val intent = Intent(this, OtherActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        myLog("onDestroy")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        myLog("onPause")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        myLog("onRestart")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        myLog("onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        myLog("onResume")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        myLog("onStop")
     }
 }
