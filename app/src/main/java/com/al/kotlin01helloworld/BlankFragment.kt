7604f9b46004c4813c81bd39a16e66e34a3a7ce0
AaronLin <1546848781@qq.com>
2022-03-29 22:39:54 +0800

    第六讲：Activity向Fragment传输数据

--- app/src/main/java/com/al/kotlin01helloworld/BlankFragment.kt
+++ app/src/main/java/com/al/kotlin01helloworld/BlankFragment.kt
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment
 
 class BlankFragment : Fragment() {
     private lateinit var tvInfo: TextView
+    private lateinit var tvArg: TextView
+    private var message: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +23,21 @@ class BlankFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_blank, container, false)
         tvInfo = root.findViewById(R.id.textViewInfo)
 
+        val argMessage = arguments?.getString(MESSAGE_KEY) ?: "没有消息"
+
+        tvArg = root.findViewById(R.id.textViewArg)
+        // 可以在这里直接操作 UI 控件
+        tvArg.text = argMessage
         return root
+    }
+
+    fun receiveMessage(message: String) {
+        // 注意此方法不要直接访问 UI 控件，外部调用此方法时此 Fragment 可能还没加载到 Activity 中
+        this.message = message
+    }
+
+    override fun onResume() {
+        super.onResume()
+        tvInfo.text = message
     }
 }
---
+++ app/src/main/java/com/al/kotlin01helloworld/FactoryMethodFragment.kt
@@ -0,0 +1,60 @@
+package com.al.kotlin01helloworld
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FactoryMethodFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FactoryMethodFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val root = inflater.inflate(R.layout.fragment_factory_method, container, false)
+        val tvFactoryMethod = root.findViewById<TextView>(R.id.textViewFactoryMethod)
+
+        tvFactoryMethod.text = param1 + param2
+        return root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment FactoryMethodFragment.
+         */
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            FactoryMethodFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
