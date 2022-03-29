e54fd136f3d0999dfeac8d556a6355026db17c59
AaronLin <1546848781@qq.com>
2022-03-29 23:16:34 +0800

    Revert "第六讲：Fragment向Activity传输数据"
    This reverts commit 9d8a7f1a

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,28 +1,15 @@
 package com.al.kotlin01helloworld
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-// Activity 必须实现监听接口
-class MainActivity : AppCompatActivity(), BlankFragment.ResponseToFragmentButtonClick {
-    private lateinit var tvInfo: TextView
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // 隐藏 ActionBar
         supportActionBar?.hide()
-        tvInfo = findViewById<TextView>(R.id.textViewInfo)
 
-        val fragment = BlankFragment()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainerView, fragment)
-            .commit()
-    }
-
-    override fun responseClick(clickCount: Int) {
-        tvInfo.text = clickCount.toString()
     }
 }
