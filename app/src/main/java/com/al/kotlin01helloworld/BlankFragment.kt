9d8a7f1a6b2282ef28be579b874235e9e37313f9
AaronLin <1546848781@qq.com>
2022-03-29 23:16:06 +0800

    第六讲：Fragment向Activity传输数据

--- app/src/main/java/com/al/kotlin01helloworld/BlankFragment.kt
+++ app/src/main/java/com/al/kotlin01helloworld/BlankFragment.kt
@@ -1,14 +1,19 @@
 package com.al.kotlin01helloworld
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class BlankFragment : Fragment() {
     private lateinit var tvInfo: TextView
+
+    // 用于引用外部监听器对象
+    private var listener: ResponseToFragmentButtonClick? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +24,27 @@ class BlankFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_blank, container, false)
-        tvInfo = root.findViewById(R.id.textViewInfo)
+        val btnSend = root.findViewById<Button>(R.id.buttonSend)
+        var count = 0
 
+        btnSend.setOnClickListener {
+            // 回调
+            listener?.responseClick(++count)
+        }
         return root
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        // 将外部 Activity 设置为监听者
+        if (context is ResponseToFragmentButtonClick) {
+            listener = context
+        } else {
+            throw RuntimeException("$context 必须实现 ResponseToFragmentButtonClick 接口")
+        }
+    }
+
+    interface ResponseToFragmentButtonClick {
+        fun responseClick(clickCount: Int)
     }
 }
--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.al.kotlin01helloworld
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+// Activity 必须实现监听接口
+class MainActivity : AppCompatActivity(), BlankFragment.ResponseToFragmentButtonClick {
+    private lateinit var tvInfo: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // 隐藏 ActionBar
         supportActionBar?.hide()
+        tvInfo = findViewById<TextView>(R.id.textViewInfo)
 
+        val fragment = BlankFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, fragment)
+            .commit()
+    }
+
+    override fun responseClick(clickCount: Int) {
+        tvInfo.text = clickCount.toString()
     }
 }
