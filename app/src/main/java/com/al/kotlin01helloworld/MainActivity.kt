6d8f263937016399cdbd0a2de069c2349fa5d1b8
AaronLin <1546848781@qq.com>
2022-03-29 20:52:44 +0800

    Revert "第六讲：Fragment添加与移除"
    This reverts commit bc7d2458

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.al.kotlin01helloworld
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private var fragmentIndex = 0
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -13,28 +11,5 @@ class MainActivity : AppCompatActivity() {
         // 隐藏 ActionBar
         supportActionBar?.hide()
 
-        var btnCreate: Button = findViewById(R.id.buttonCreate)
-        var btnRemove: Button = findViewById(R.id.buttonRemove)
-
-        btnCreate.setOnClickListener {
-            // 实例化 Fragment
-            val name = "BlankFragment ${++fragmentIndex}"
-            val fragment = MyFirstFragment(name)
-            // 添加到 Activity 中
-            supportFragmentManager.beginTransaction()
-                // 添加到栈中，可以通过返回键，回退到上一个 Fragment
-                .addToBackStack(null)
-                .add(R.id.fragmentContainerView, fragment, "blank")
-                .commit()
-        }
-
-        btnRemove.setOnClickListener {
-            val fragment = supportFragmentManager.findFragmentByTag("blank")
-            if (fragment != null) {
-                supportFragmentManager.beginTransaction()
-                    .remove(fragment)
-                    .commit()
-            }
-        }
     }
 }
