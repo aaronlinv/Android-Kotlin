af726f99027645604128b6cffdf535e925b7d1ab
AaronLin <1546848781@qq.com>
2022-03-29 21:01:07 +0800

    Revert "第六讲：Fragment生命周期"
    This reverts commit 226938cb

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.al.kotlin01helloworld
 
-import android.app.Fragment
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,42 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // 隐藏 ActionBar
         supportActionBar?.hide()
-        myLog("Activity onCreate")
-        val fragment = BlankFragment()
 
-        supportFragmentManager.beginTransaction()
-
-            .add(R.id.fragmentContainerView, fragment)
-            .commit()
-
-        supportFragmentManager.addFragmentOnAttachListener { fragmentManager, fragment ->
-            myLog("Fragment ${fragment.id} is attached")
-        }
-    }
-
-    // 已废弃，用上面的方式代替
-    override fun onAttachFragment(fragment: Fragment?) {
-        super.onAttachFragment(fragment)
-        myLog("Activity onAttachFragment")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        myLog("Activity onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        myLog("Activity onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        myLog("Activity onPause")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        myLog("Activity onDestroy")
     }
 }
