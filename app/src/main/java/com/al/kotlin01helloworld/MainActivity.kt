7769f7718cb05aaac072e428d2d76bf81d36d4bf
AaronLin <1546848781@qq.com>
2022-03-30 23:54:10 +0800

    第七讲：MediatorLiveData

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.al.kotlin01helloworld
 
 import android.os.Bundle
-import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var tvCount: TextView
-    private lateinit var btnAdd: Button
-    private val myClass = MyClass()
+    private lateinit var tvInfo: TextView
+    private lateinit var etFirst: EditText
+    private lateinit var etSecond: EditText
+
+    private val string1 = MutableLiveData<String>()
+    private val string2 = MutableLiveData<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -16,13 +23,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // 隐藏 ActionBar
         supportActionBar?.hide()
-        tvCount = findViewById(R.id.tvCount)
-        btnAdd = findViewById(R.id.btnAdd)
-        btnAdd.setOnClickListener {
-            myClass.addCount()
+        tvInfo = findViewById(R.id.tvInfo)
+        etFirst = findViewById(R.id.etFirst)
+        etSecond = findViewById(R.id.etSecond)
+
+        initEditText()
+        initMediatorLiveData()
+    }
+
+    private fun initEditText() {
+        etFirst.addTextChangedListener {
+            string1.value = it.toString()
         }
-        myClass.counter.observe(this) {
-            tvCount.text = it.toString()
+        etSecond.addTextChangedListener {
+            string2.value = it.toString()
+        }
+    }
+
+    private fun initMediatorLiveData() {
+        val result = MediatorLiveData<Int>()
+        val doSum = Observer<String> {
+            val len1 = string1.value?.length ?: 0
+            val len2 = string2.value?.length ?: 0
+            result.value = len1 + len2
+        }
+        // 任一变化都会调用 doSum 方法
+        result.addSource(string1, doSum)
+        result.addSource(string2, doSum)
+
+        result.observe(this) {
+            tvInfo.text = result.value.toString() ?: "0"
         }
     }
 }
