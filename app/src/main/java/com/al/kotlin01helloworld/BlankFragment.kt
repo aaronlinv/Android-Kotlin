386b625c2249e84f622d66e47db4849ac58a9087
AaronLin <1546848781@qq.com>
2022-03-30 20:08:06 +0800

    Revert "第六讲：Fragment向Fragment传输数据"
    This reverts commit ebf7c780

--- app/src/main/java/com/al/kotlin01helloworld/BlankFragment.kt
+++ app/src/main/java/com/al/kotlin01helloworld/BlankFragment.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class BlankFragment : Fragment() {
-    private lateinit var btnSend: Button
-    private lateinit var etMessage: EditText
+    private lateinit var tvInfo: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +19,8 @@ class BlankFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_blank, container, false)
-        btnSend = root.findViewById(R.id.buttonSend)
-        etMessage = root.findViewById(R.id.editTextMessage)
+        tvInfo = root.findViewById(R.id.textViewInfo)
 
-        btnSend.setOnClickListener {
-            val message = Bundle()
-            message.putString(MESSAGE_KEY, etMessage.text.toString())
-            // 通过 Activity 定义的共有方法中转
-            (activity as MainActivity)?.switchFragment(SHOW_FRAGMENT, message)
-        }
         return root
     }
 }
