3e837dbefc798d7655bfb76c20138df9025fbdc8
AaronLin <1546848781@qq.com>
2022-03-30 21:08:07 +0800

    第七讲：LifeDate

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.al.kotlin01helloworld
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvCount: TextView
+    private lateinit var btnAdd: Button
+    private val myClass = MyClass()
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // 隐藏 ActionBar
         supportActionBar?.hide()
-
+        tvCount = findViewById(R.id.tvCount)
+        btnAdd = findViewById(R.id.btnAdd)
+        btnAdd.setOnClickListener {
+            myClass.addCount()
+        }
+        myClass.counter.observe(this) {
+            tvCount.text = it.toString()
+        }
     }
 }
