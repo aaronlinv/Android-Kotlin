96a92eba265243bad68af74e3c656ad7d7f7c9cc
AaronLin <1546848781@qq.com>
2022-03-31 20:29:47 +0800

    第七讲：ViewModel

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     private lateinit var tvCount: TextView
     private lateinit var btnAdd: Button
+    private lateinit var viewModel: MyViewModel
     private val myClass = MyClass()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,12 +18,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // 隐藏 ActionBar
         supportActionBar?.hide()
+
+        viewModel = ViewModelProvider(this)
+            .get(MyViewModel::class.java)
+
         tvCount = findViewById(R.id.tvCount)
         btnAdd = findViewById(R.id.btnAdd)
         btnAdd.setOnClickListener {
-            myClass.addCount()
+            val initValue = viewModel.score.value!!
+            viewModel.changeScore(initValue + 1)
         }
-        myClass.counter.observe(this) {
+        viewModel.score.observe(this) {
             tvCount.text = it.toString()
         }
     }
