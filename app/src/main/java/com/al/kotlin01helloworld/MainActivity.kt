4b34c3b87a792bc96e7b0e3d0075e2511d37ebcc
AaronLin <1546848781@qq.com>
2022-04-01 21:34:58 +0800

    Revert "第七讲：Activity通过ViewModel向Fragment传递消息"
    This reverts commit 06ee6b5d

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -2,11 +2,15 @@ package com.al.kotlin01helloworld
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvCount: TextView
     private lateinit var btnAdd: Button
-    lateinit var viewModel: MyViewModel
+    private lateinit var viewModel: MyViewModel
+    private val myClass = MyClass()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -15,23 +19,17 @@ class MainActivity : AppCompatActivity() {
         // 隐藏 ActionBar
         supportActionBar?.hide()
 
+        viewModel = ViewModelProvider(this)
+            .get(MyViewModel::class.java)
+
+        tvCount = findViewById(R.id.tvCount)
         btnAdd = findViewById(R.id.btnAdd)
         btnAdd.setOnClickListener {
             val initValue = viewModel.score.value!!
             viewModel.changeScore(initValue + 1)
         }
-        val blankFragment = BlankFragment()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainerView, blankFragment, "blank_fragment")
-            .commit()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        // 注意要在 onStart 才可以找到对应 Fragment
-        // 在xml 中定义 id 和 tag 似乎不太管用
-        val blankFragment = supportFragmentManager.findFragmentByTag("blank_fragment")
-        // val blankFragment = supportFragmentManager.findFragmentById(R.id.blankFragment)
-        viewModel = (blankFragment as BlankFragment).viewModel
+        viewModel.score.observe(this) {
+            tvCount.text = it.toString()
+        }
     }
 }
