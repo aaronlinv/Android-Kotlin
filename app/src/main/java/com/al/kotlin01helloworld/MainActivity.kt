f31c98c43a5da3bf5d071007909d30bc5bcc0ee5
AaronLin <1546848781@qq.com>
2022-04-01 23:43:16 +0800

    第八讲：NavGroup与Menu栏跳转

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.al.kotlin01helloworld
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.onNavDestinationSelected
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnAdd: Button
@@ -15,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // 隐藏 ActionBar
-        supportActionBar?.hide()
+        // supportActionBar?.hide()
         btnAdd = findViewById(R.id.btnNavigate)
         val hostFragment: NavHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
@@ -27,7 +30,17 @@ class MainActivity : AppCompatActivity() {
             // navController.navigate(R.id.secondFragment)
 
             // 还可以跳转 Activity
-            navController.navigate(R.id.otherActivity)
+            navController.navigate(R.id.navGroup)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.my_nav_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // item 定义的 id 需要和 my_nav_graph 目的地定义的 id 相同，否则无法跳转
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 }
