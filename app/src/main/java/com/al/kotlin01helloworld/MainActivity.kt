a16e950b407ddfb640abc6a3446884fadb1e4147
AaronLin <1546848781@qq.com>
2022-04-06 00:13:39 +0800

    第八讲：底层导航栏

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -2,6 +2,9 @@ package com.al.kotlin01helloworld
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +13,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // 隐藏 ActionBar
         supportActionBar?.hide()
+        // val navController = findNavController(R.id.fragmentContainerView)
+        // val buttonNavigation: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        // buttonNavigation.setupWithNavController(navController)
+    }
 
+    override fun onStart() {
+        super.onStart()
+
+        val navController = findNavController(R.id.fragmentContainerView)
+        val buttonNavigation: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        // 自动导航
+        buttonNavigation.setupWithNavController(navController)
     }
 }
