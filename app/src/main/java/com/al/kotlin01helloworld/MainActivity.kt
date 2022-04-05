93db514c66785c1d1ba213886d9a23469da05c31
AaronLin <1546848781@qq.com>
2022-04-05 23:42:28 +0800

    Revert "第八讲：ToolBar"

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,18 +1,14 @@
 package com.al.kotlin01helloworld
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
-    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -20,23 +16,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // 隐藏 ActionBar
         // supportActionBar?.hide()
-        // toolbar 取代 ActionBar
-        toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.my_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // 切换显示不同的 Fragment
-        when (item.itemId) {
-            R.id.firstFragment2 -> navController.navigate(R.id.firstFragment2)
-            R.id.secondFragment2 -> navController.navigate(R.id.secondFragment2)
-        }
-        return super.onOptionsItemSelected(item)
     }
 
     override fun onStart() {
@@ -44,14 +23,12 @@ class MainActivity : AppCompatActivity() {
         // onStart(): Activity 中获取，需要 Fragment 装载完毕才可以
         navController = findNavController(R.id.fragmentContainerView)
 
-        // 三个 Fragment 没有主次之分
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.firstFragment2,
-                R.id.secondFragment2
-            )
-        )
-        toolbar.setupWithNavController(navController, appBarConfiguration)
+        // navController 与 ActionBar 绑定
+        setupActionBarWithNavController(navController)
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            val info = "导航到了 ${destination.label}"
+            Toast.makeText(this, info, Toast.LENGTH_SHORT).show()
+        }
     }
 
     // 返回按钮具体功能
