c6db390ecd0da52c0bdc687c2f7193529f3ba6ec
AaronLin <1546848781@qq.com>
2022-04-06 21:04:21 +0800

    Revert "第九讲：ViewPager2" Revert "第九讲：ViewPager2+Holder"

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,28 +1,28 @@
 package com.al.kotlin01helloworld
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var pager: ViewPager2
+    private lateinit var tvCount: TextView
+    private lateinit var btnAdd: Button
+    private val myClass = MyClass()
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // 隐藏 ActionBar
         supportActionBar?.hide()
-
-        pager = findViewById(R.id.viewPager)
-        // 实例化适配器对象给 ViewPager2
-        pager.adapter = ImageViewPagerAdapter(createImageList())
-
-    }
-
-    private fun createImageList(): List<ImageItem> {
-        return listOf(
-            ImageItem("Forest", R.drawable.forst),
-            ImageItem("Mountain", R.drawable.mountain)
-        )
+        tvCount = findViewById(R.id.tvCount)
+        btnAdd = findViewById(R.id.btnAdd)
+        btnAdd.setOnClickListener {
+            myClass.addCount()
+        }
+        myClass.counter.observe(this) {
+            tvCount.text = it.toString()
+        }
     }
 }
