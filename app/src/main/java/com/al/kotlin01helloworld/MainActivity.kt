0a065826f0a74789ef40ca7a704ca013e6d1a3b7
AaronLin <1546848781@qq.com>
2022-04-06 23:41:50 +0800

    第九讲：BindingAdapter配合Glide

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -2,7 +2,6 @@ package com.al.kotlin01helloworld
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import com.al.kotlin01helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,17 +13,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         // 隐藏 ActionBar
         supportActionBar?.hide()
-        val myName = MyName("宋江", "及时雨")
+        val photoUrl =
+            "https://tenfei04.cfp.cn/creative/vcg/800/new/VCG211177178924.jpg"
+        binding.imageUrl = ""
 
-        //     with(binding) {
-        //         tvName.text = myName.name
-        //         tvNickname.text = myName.nickname
-        //     }
-        var dataBinding: ActivityMainBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
-        dataBinding.myName = myName
-        // 直接用视图绑定的对象也可以
-        // binding.myName = myName
-
+        binding.btnLoad.setOnClickListener {
+            binding.imageUrl = photoUrl
+        }
     }
 }
