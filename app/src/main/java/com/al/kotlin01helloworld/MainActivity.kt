82fea5701bbe6a9f469f64b7e5884b9feb819211
AaronLin <1546848781@qq.com>
2022-04-06 20:08:37 +0800

    第九讲：ViewPager2

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,28 +1,20 @@
 package com.al.kotlin01helloworld
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var tvCount: TextView
-    private lateinit var btnAdd: Button
-    private val myClass = MyClass()
-
+    private lateinit var pager: ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // 隐藏 ActionBar
         supportActionBar?.hide()
-        tvCount = findViewById(R.id.tvCount)
-        btnAdd = findViewById(R.id.btnAdd)
-        btnAdd.setOnClickListener {
-            myClass.addCount()
-        }
-        myClass.counter.observe(this) {
-            tvCount.text = it.toString()
-        }
+
+        pager = findViewById(R.id.viewPager)
+        // 实例化适配器对象给 ViewPager2
+        pager.adapter = MyPageAdapter(this)
     }
 }
