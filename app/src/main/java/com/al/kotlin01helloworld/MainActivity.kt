2c581e3033afa6415dbcbb19c0eb23b8fb5912de
AaronLin <1546848781@qq.com>
2022-04-06 19:25:04 +0800

    Revert "第八讲：Navigation跳转Fragment时通过Bundle传递数据" Revert "第八讲：底层导航栏" Revert "第八讲：底层导航栏（手动导航）"

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,38 +1,28 @@
 package com.al.kotlin01helloworld
 
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvCount: TextView
+    private lateinit var btnAdd: Button
+    private val myClass = MyClass()
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // 隐藏 ActionBar
         supportActionBar?.hide()
-        // val navController = findNavController(R.id.fragmentContainerView)
-        // val buttonNavigation: BottomNavigationView = findViewById(R.id.bottomNavigationView)
-        // buttonNavigation.setupWithNavController(navController)
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        val navController = findNavController(R.id.fragmentContainerView)
-        val buttonNavigation: BottomNavigationView = findViewById(R.id.bottomNavigationView)
-        // 自动导航
-        // buttonNavigation.setupWithNavController(navController)
-        // 手动导航
-        buttonNavigation.setOnItemReselectedListener {
-            when (it.itemId) {
-                R.id.firstFragment2 -> Toast.makeText(this, "firstFragment2", Toast.LENGTH_SHORT)
-                    .show()
-                R.id.secondFragment2 -> Toast.makeText(this, "secondFragment2", Toast.LENGTH_SHORT)
-                    .show()
-            }
+        tvCount = findViewById(R.id.tvCount)
+        btnAdd = findViewById(R.id.btnAdd)
+        btnAdd.setOnClickListener {
+            myClass.addCount()
+        }
+        myClass.counter.observe(this) {
+            tvCount.text = it.toString()
         }
     }
 }
