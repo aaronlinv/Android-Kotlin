e2f9564db0852882cf6ea5cc23c4e97f774a24a3
AaronLin <1546848781@qq.com>
2022-04-11 00:14:18 +0800

    恢复为普通Activity

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -6,25 +6,13 @@ import androidx.databinding.DataBindingUtil
 import com.al.kotlin01helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var dataBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         // 隐藏 ActionBar
         supportActionBar?.hide()
-        val myName = MyName("宋江", "及时雨")
 
-        //     with(binding) {
-        //         tvName.text = myName.name
-        //         tvNickname.text = myName.nickname
-        //     }
-        var dataBinding: ActivityMainBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
-        dataBinding.myName = myName
-        // 直接用视图绑定的对象也可以
-        // binding.myName = myName
-
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
