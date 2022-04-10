04e96c49fcf787e946cc961df3d396932e75c560
AaronLin <1546848781@qq.com>
2022-04-11 00:10:47 +0800

    Revert "第十讲：古诗RecycleView案例"
    This reverts commit 22d51477

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,65 +1,30 @@
 package com.al.kotlin01helloworld
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.al.kotlin01helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var adapter: PoemAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         // 隐藏 ActionBar
-        // supportActionBar?.hide()
+        supportActionBar?.hide()
+        val myName = MyName("宋江", "及时雨")
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        //     with(binding) {
+        //         tvName.text = myName.name
+        //         tvNickname.text = myName.nickname
+        //     }
+        var dataBinding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        dataBinding.myName = myName
+        // 直接用视图绑定的对象也可以
+        // binding.myName = myName
 
-        val spacingInPixels = 8
-        binding.rvPoem.addItemDecoration(SpacesItemDecoration(spacingInPixels))
-        adapter = PoemAdapter(PoemLibrary.getPoemList())
-        binding.rvPoem.layoutManager = LinearLayoutManager(this)
-
-        binding.rvPoem.adapter = adapter
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        super.onCreateOptionsMenu(menu)
-        menuInflater.inflate(R.menu.layout_manager, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        super.onOptionsItemSelected(item)
-        when (item.itemId) {
-            R.id.mnuGridLayout -> changeToGridLayout()
-            R.id.mnuLinerLayout -> changeToLinerLayout()
-            R.id.mnuStaggerLayout -> changeToStaggerLayout()
-        }
-        return true
-    }
-
-    private fun changeToGridLayout() {
-        binding.rvPoem.layoutManager = GridLayoutManager(this, 2)
-        binding.rvPoem.adapter = adapter
-    }
-
-    private fun changeToLinerLayout() {
-        binding.rvPoem.layoutManager = LinearLayoutManager(this)
-        binding.rvPoem.adapter = adapter
-    }
-
-    private fun changeToStaggerLayout() {
-        binding.rvPoem.layoutManager =
-            StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-        binding.rvPoem.adapter = adapter
     }
 }
