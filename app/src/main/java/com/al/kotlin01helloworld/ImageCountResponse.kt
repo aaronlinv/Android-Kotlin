1419eab72060379ac07f77952d9c1ee1719a7431
AaronLin <1546848781@qq.com>
2022-04-12 00:42:57 +0800

    第十讲：传统方式使用Retrofit

---
+++ app/src/main/java/com/al/kotlin01helloworld/ImageCountResponse.kt
@@ -0,0 +1,13 @@
+package com.al.kotlin01helloworld
+
+/**
+ * <pre>
+ *     author : Aaron
+ *     time   : 2022/04/11
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+data class ImageCountResponse(
+    var imageCount: Int
+)
--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,31 +1,79 @@
 package com.al.kotlin01helloworld
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.al.kotlin01helloworld.databinding.ActivityMainBinding
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var dataBinding: ActivityMainBinding
+    private val imageServer: MyImageServer = MyImageServer.imageServer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // 隐藏 ActionBar
         supportActionBar?.hide()
-
         dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         with(dataBinding) {
-            refreshLayout.setOnRefreshListener {
-                tvInfo.text = "正在刷新"
-                btnRefresh.isEnabled = true
+
+        }
+        dataBinding.btnLoad.setOnClickListener {
+            getNewestImageUseCallback()
+        }
+    }
+
+    private fun getNewestImageUseCallback() {
+        val call = imageServer.getImageCount()
+        // Retrofit 采用队列的方式
+        call.enqueue(object : Callback<ImageCountResponse> {
+            override fun onResponse(
+                call: Call<ImageCountResponse>,
+                response: Response<ImageCountResponse>
+            ) {
+                val result = response.body()
+                if (result != null) {
+                    val imageName = "image_${result.imageCount}.jpg"
+                    myLog("拼接后的图片名称：${imageName}")
+
+                    val call2 = imageServer.getImage(imageName)
+                    call2.enqueue(object : Callback<ResponseBody> {
+                        override fun onResponse(
+                            call: Call<ResponseBody>,
+                            response: Response<ResponseBody>
+                        ) {
+                            val stream = response.body()?.byteStream()
+                            val bitmap: Bitmap?
+                            stream.use {
+                                bitmap = BitmapFactory.decodeStream(stream)
+                            }
+                            runOnUiThread {
+                                dataBinding.ivImage.setImageBitmap(bitmap)
+                            }
+                        }
+
+                        override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                            showInfo(t.message!!)
+                        }
+
+                    })
+                }
             }
 
-            btnRefresh.setOnClickListener {
-                tvInfo.text = "下拉刷新"
-                refreshLayout.isRefreshing = false
-                btnRefresh.isEnabled = false
+            override fun onFailure(call: Call<ImageCountResponse>, t: Throwable) {
+                showInfo(t.message!!)
             }
-        }
+
+        })
+    }
+
+    private fun showInfo(message: String) {
+        showToast(this, message)
     }
 }
