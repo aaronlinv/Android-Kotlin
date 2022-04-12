cc7fcd1fa226013c36e18ef4872713747a23b725
AaronLin <1546848781@qq.com>
2022-04-12 22:38:40 +0800

    Revert "第十讲：传统方式使用Retrofit" Revert "第十讲：协程方式使用Retrofit" Revert "第十讲：图片批量下载案例"

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,156 +1,31 @@
 package com.al.kotlin01helloworld
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.lifecycleScope
 import com.al.kotlin01helloworld.databinding.ActivityMainBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import okhttp3.ResponseBody
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var dataBinding: ActivityMainBinding
-    private val imageServer: MyImageServer = MyImageServer.imageServer
-    private var downloadCounter: Int = 0
-    private val images: MutableList<Bitmap> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // 隐藏 ActionBar
         supportActionBar?.hide()
+
         dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        dataBinding.btnLoad.setOnClickListener {
-            // getNewestImageUseCallback()
-            // getNewestImageUseCoroutine()
-            if (images.size > 0) {
-                val index = downloadCounter % images.size
-                dataBinding.tvUrl.text = "显示第 ${index + 1} 张图片"
-                dataBinding.ivImage.setImageBitmap(images[index])
-                downloadCounter++
-            } else {
-                it.isEnabled = false
-                downloadImage()
+        with(dataBinding) {
+            refreshLayout.setOnRefreshListener {
+                tvInfo.text = "正在刷新"
+                btnRefresh.isEnabled = true
+            }
+
+            btnRefresh.setOnClickListener {
+                tvInfo.text = "下拉刷新"
+                refreshLayout.isRefreshing = false
+                btnRefresh.isEnabled = false
             }
         }
-    }
-
-    private fun getNewestImageUseCallback() {
-        val call = imageServer.getImageCount()
-        // Retrofit 采用队列的方式
-        call.enqueue(object : Callback<ImageCountResponse> {
-            override fun onResponse(
-                call: Call<ImageCountResponse>,
-                response: Response<ImageCountResponse>
-            ) {
-                val result = response.body()
-                if (result != null) {
-                    val imageName = "image_${result.imageCount}.jpg"
-                    myLog("拼接后的图片名称：${imageName}")
-
-                    val call2 = imageServer.getImage(imageName)
-                    call2.enqueue(object : Callback<ResponseBody> {
-                        override fun onResponse(
-                            call: Call<ResponseBody>,
-                            response: Response<ResponseBody>
-                        ) {
-                            val stream = response.body()?.byteStream()
-                            val bitmap: Bitmap?
-                            stream.use {
-                                bitmap = BitmapFactory.decodeStream(stream)
-                            }
-                            runOnUiThread {
-                                dataBinding.ivImage.setImageBitmap(bitmap)
-                            }
-                        }
-
-                        override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                            showInfo(t.message!!)
-                        }
-
-                    })
-                }
-            }
-
-            override fun onFailure(call: Call<ImageCountResponse>, t: Throwable) {
-                showInfo(t.message!!)
-            }
-
-        })
-    }
-
-    // 异步任务
-    private suspend fun getImageItem(): Int {
-        // 切换到 IO 线程
-        return withContext(Dispatchers.IO) {
-            imageServer.getImageCount2().body()?.imageCount ?: 0
-        }
-    }
-
-    private suspend fun getImageUseCoroutine(imageName: String): Bitmap? {
-        return withContext(Dispatchers.IO) {
-            val result = imageServer.getImage2(imageName)
-            var bitmap: Bitmap? = null
-            // use 关闭的时候自动关闭流
-            result.body()?.byteStream()?.use {
-                bitmap = BitmapFactory.decodeStream(it)
-            }
-            bitmap
-        }
-    }
-
-    private fun getNewestImageUseCoroutine() {
-        // Lifecycle 被销毁时，运行的任务会被取消
-        lifecycleScope.launch {
-            val imageCount = getImageItem()
-            if (imageCount != 0) {
-                val imageName = "image_$imageCount.jpg"
-                myLog("图片：$imageName")
-                dataBinding.tvUrl.text = imageName
-                val bitmap = getImageUseCoroutine(imageName)
-                dataBinding.ivImage.setImageBitmap(bitmap)
-            }
-        }
-    }
-
-    private fun downloadImage() {
-        lifecycleScope.launch {
-            val imageCount = getImageItem()
-            if (imageCount > 0) {
-                repeat(imageCount) {
-                    var imageName = ""
-                    val index = it + 1
-                    if (index < 10) {
-                        imageName = "image_0$index.jpg"
-                    } else {
-                        imageName = "image_$index.jpg"
-                    }
-                    myLog("图片：$imageName")
-
-                    val bitmap = async {
-                        getImageUseCoroutine(imageName)
-                    }.await()
-                    bitmap?.let {
-                        images.add(it)
-                        dataBinding.tvUrl.text = "已下载图片：${++downloadCounter}"
-                    }
-                }
-                dataBinding.tvUrl.text = "图片下载完毕"
-                dataBinding.btnLoad.text = "切换显示图片"
-                dataBinding.btnLoad.isEnabled = true
-            }
-        }
-    }
-
-    private fun showInfo(message: String) {
-        showToast(this, message)
     }
 }
