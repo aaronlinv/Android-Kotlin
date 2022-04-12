b273a35627bd1d34fda6559d7cb44ee8e6c04b0f
AaronLin <1546848781@qq.com>
2022-04-12 23:34:53 +0800

    第十讲：协程计算UI控件显示进度

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.al.kotlin01helloworld.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var dataBinding: ActivityMainBinding
+    private var job: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,16 +18,50 @@ class MainActivity : AppCompatActivity() {
         dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         with(dataBinding) {
-            refreshLayout.setOnRefreshListener {
-                tvInfo.text = "正在刷新"
-                btnRefresh.isEnabled = true
-            }
+            val threadName = Thread.currentThread().name
+            tvInfo.text = threadName
 
             btnRefresh.setOnClickListener {
-                tvInfo.text = "下拉刷新"
-                refreshLayout.isRefreshing = false
-                btnRefresh.isEnabled = false
+                job = MainScope().launch {
+                    it.isEnabled = false
+                    tvInfo.text = "协程启动..."
+                    // 自动切换到后台执行计算，同时显示进度
+                    val result = calculateUseCoroutine()
+                    // 挂起函数后，会切换回主线程，执行下面代码
+                    tvInfo.text = "计算的结果为：$result，协程：${job}"
+                    it.isEnabled = true
+                }
             }
         }
+    }
+
+    private suspend fun calculateUseCoroutine(): Long {
+        // CPU 密集型使用 Default
+        return withContext(Dispatchers.Default) {
+            var sum: Long = 0
+            for (i in 1..100) {
+                sum += i
+                delay(80)
+                if (i % 10 == 0) {
+                    showProgress(i)
+                    myLog("正在处理 ${i},${Thread.currentThread().name}")
+                }
+            }
+            sum
+        }
+    }
+
+    // UI 线程
+    private suspend fun showProgress(percent: Int) {
+        withContext(Dispatchers.Main) {
+            dataBinding.tvInfo.text = percent.toString()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        job?.cancel()
+        myLog("onStop，协程被取消：$job")
+        dataBinding.btnRefresh.isEnabled = true
     }
 }
