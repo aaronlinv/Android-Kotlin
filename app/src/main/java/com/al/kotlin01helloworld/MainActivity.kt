ee2e2e0c0cef002b825f06ce2544a72b6adceabf
AaronLin <1546848781@qq.com>
2022-04-14 22:50:49 +0800

    第十一讲：协程配合ViewModel进行后台任务

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,30 +1,54 @@
 package com.al.kotlin01helloworld
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.al.kotlin01helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var dataBinding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // 隐藏 ActionBar
         supportActionBar?.hide()
 
-        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        with(dataBinding) {
-            refreshLayout.setOnRefreshListener {
-                tvInfo.text = "正在刷新"
-                btnRefresh.isEnabled = true
+        val viewModel = ViewModelProvider(this)
+            .get(MainViewModel::class.java)
+        with(binding) {
+            btnRun.setOnClickListener {
+                if (viewModel.currentState.value != WorkState.WORKING) {
+                    viewModel.doWork()
+                } else {
+                    viewModel.cancel()
+                }
             }
+        }
 
-            btnRefresh.setOnClickListener {
-                tvInfo.text = "下拉刷新"
-                refreshLayout.isRefreshing = false
-                btnRefresh.isEnabled = false
+        viewModel.workingInfo.observe(this) {
+            binding.tvInfo.text = it
+        }
+        viewModel.currentState.observe(this) {
+            when (it) {
+                WorkState.WORKING -> {
+                    binding.pgWorkingr.visibility = View.VISIBLE
+                    binding.btnRun.text = "取消"
+                }
+
+                WorkState.IDLE -> {
+                    binding.pgWorkingr.visibility = View.INVISIBLE
+                    binding.btnRun.text = "运行"
+                }
+
+                WorkState.CANCELED -> {
+                    binding.pgWorkingr.visibility = View.INVISIBLE
+                    binding.tvInfo.text = "取消"
+                    binding.btnRun.text = "运行"
+                }
             }
         }
     }
