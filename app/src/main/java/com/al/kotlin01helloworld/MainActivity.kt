a5b38af2fa6f78f5caac61f3f0fc91c0ec9733f6
AaronLin <1546848781@qq.com>
2022-04-16 12:51:00 +0800

    第十一讲：SharedPreferences早期写法

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.al.kotlin01helloworld
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -7,6 +8,10 @@ import com.al.kotlin01helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var dataBinding: ActivityMainBinding
+
+    val PREF_FILE_NAME = "mydata"
+    val NAME_FILED = "name"
+    val AGE_FILED = "age"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,15 +21,27 @@ class MainActivity : AppCompatActivity() {
         dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         with(dataBinding) {
-            refreshLayout.setOnRefreshListener {
-                tvInfo.text = "正在刷新"
-                btnRefresh.isEnabled = true
-            }
+            btnLoad.setOnClickListener {
+                val pref = getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE)
+                val name = pref.getString(NAME_FILED, "")
+                val age = pref.getInt(AGE_FILED, 0)
+                dataBinding.tvInfo.text = "姓名：$name 年龄：$age"
 
-            btnRefresh.setOnClickListener {
-                tvInfo.text = "下拉刷新"
-                refreshLayout.isRefreshing = false
-                btnRefresh.isEnabled = false
+                // 遍历
+                pref.all.forEach {
+                    myLog("${it.key} --> ${it.value}")
+                }
+            }
+            btnSave.setOnClickListener {
+                val pref = getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE)
+                // 1. 获取编辑器对象
+                val editor = pref.edit()
+                // 2. 写入数据
+                editor.putString(NAME_FILED, "测试名称")
+                editor.putInt(AGE_FILED, 18)
+                // 3. 提交
+                editor.apply()
+                dataBinding.tvInfo.text = "数据已经保存"
             }
         }
     }
