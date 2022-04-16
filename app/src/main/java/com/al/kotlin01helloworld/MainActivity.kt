f3e4362da72d4c0a1adc46fc0b51695a06a88a32
AaronLin <1546848781@qq.com>
2022-04-16 20:58:17 +0800

    第十二讲：SQLiteOpenHelper操作数据库

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.al.kotlin01helloworld
 
-import android.database.Cursor
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.al.kotlin01helloworld.databinding.ActivityMainBinding
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var dataBinding: ActivityMainBinding
-
+    private var dbHelper = MyDBHelper(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // 隐藏 ActionBar
@@ -21,52 +18,33 @@ class MainActivity : AppCompatActivity() {
         dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         with(dataBinding) {
-            btnCreate.setOnClickListener {
+            tvInfo.text = "数据库版本：${MyDBHelper.VERSION}"
+
+            btnInsert.setOnClickListener {
                 MainScope().launch {
-                    createDBUseSQL()
+                    val db = dbHelper.writableDatabase
+                    val insertResult = if (MyDBHelper.VERSION == 1) {
+                        dbHelper.insertData(db, "First", 18)
+                    } else {
+                        dbHelper.insertData2(db, "First", 18, 20)
+                    }
+                    // db.close()
+                    tvInfo.text = insertResult.toString()
                 }
             }
             btnShow.setOnClickListener {
                 MainScope().launch {
-                    showDataInDBUseSQL()
+                    val db = dbHelper.readableDatabase
+                    val result = dbHelper.query(db)
+                    // db.close()
+                    tvInfo.text = result
                 }
             }
         }
     }
 
-    // 使用 SQL 直接创建数据库
-    private suspend fun createDBUseSQL() {
-        withContext(Dispatchers.IO) {
-            val db = openOrCreateDatabase("my_sqlite_db", MODE_PRIVATE, null)
-            db.execSQL("DROP TABLE IF EXISTS myTable")
-            db.execSQL("CREATE TABLE IF NOT EXISTS myTable(TEXT info,INTEGER value)")
-            db.execSQL("INSERT INTO myTable VALUES ('INFO',18)")
-            db.execSQL("INSERT INTO myTable VALUES ('这是一个中文信息',200)")
-            // 如果 close 了，那么 AS 的 App Inspection 中会无法使用
-            db.close()
-        }
-        dataBinding.tvInfo.text = "数据库已创建"
-    }
-
-    private suspend fun showDataInDBUseSQL() {
-        val result = withContext(Dispatchers.IO) {
-            val db = openOrCreateDatabase("my_sqlite_db", MODE_PRIVATE, null)
-            db.use {
-                val query: Cursor = db.rawQuery("SELECT * FROM myTable", null)
-                val sb = StringBuilder()
-                if (query.moveToFirst()) {
-                    do {
-                        val info = query.getString(0)
-                        val value = query.getInt(1)
-                        sb.append("$info $value \n")
-                    } while (query.moveToNext())
-                } else {
-                    sb.append("没有数据")
-                }
-                query.close()
-                sb.toString()
-            }
-        }
-        dataBinding.tvInfo.text = result
+    override fun onDestroy() {
+        super.onDestroy()
+        dbHelper.close()
     }
 }
