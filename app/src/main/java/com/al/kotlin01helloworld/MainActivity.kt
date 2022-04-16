b7e224e3d3b82eb1b913d69f8ef907800a88b407
AaronLin <1546848781@qq.com>
2022-04-16 14:45:15 +0800

    Revert "第十一讲：SharedPreferences早期写法" Revert "第十一讲：DataStore存储SharedPreferences"

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -1,34 +1,12 @@
 package com.al.kotlin01helloworld
 
-import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.intPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
 import com.al.kotlin01helloworld.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
-import kotlin.random.Random
-
-private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
-    name = "settings"
-)
 
 class MainActivity : AppCompatActivity() {
     private lateinit var dataBinding: ActivityMainBinding
-
-    val PREF_FILE_NAME = "mydata"
-    val NAME_FILED = "name"
-    val AGE_FILED = "age"
-    val ITEM_KEY = intPreferencesKey("my_int_number")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,25 +16,15 @@ class MainActivity : AppCompatActivity() {
         dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         with(dataBinding) {
-            btnLoad.setOnClickListener {
-
-                MainScope().launch {
-                    val num = dataStore.data.map {
-                        it[ITEM_KEY]
-                    }.firstOrNull()
-                    dataBinding.tvInfo.text = num?.toString() ?: "获取数据失败"
-                }
+            refreshLayout.setOnRefreshListener {
+                tvInfo.text = "正在刷新"
+                btnRefresh.isEnabled = true
             }
-            btnSave.setOnClickListener {
 
-                val ranValue = Random.nextInt(1, 100)
-                CoroutineScope(Dispatchers.IO).launch {
-                    dataStore.edit {
-                        it[ITEM_KEY] = ranValue
-                    }
-                }
-
-                dataBinding.tvInfo.text = "将 $ranValue 写入"
+            btnRefresh.setOnClickListener {
+                tvInfo.text = "下拉刷新"
+                refreshLayout.isRefreshing = false
+                btnRefresh.isEnabled = false
             }
         }
     }
