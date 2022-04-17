a88fb484645eff7b8ade910f8a466e95493d91ec
AaronLin <1546848781@qq.com>
2022-04-18 00:37:30 +0800

    Revert "第十二讲：Room案例" Revert "第十二讲：Room结合Coroutine" Revert "第十二讲：Room配合ViewModel"

--- app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
+++ app/src/main/java/com/al/kotlin01helloworld/MainActivity.kt
@@ -3,41 +3,10 @@ package com.al.kotlin01helloworld
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.room.Room
 import com.al.kotlin01helloworld.databinding.ActivityMainBinding
-import com.al.kotlin01helloworld.db.MyDatabase
-import com.al.kotlin01helloworld.vm.MainViewModel
-import com.al.kotlin01helloworld.vm.MyViewModelFactory
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
-
-private val MainActivity.viewModelProvider: ViewModelProvider
-    get() = ViewModelProvider(this)
-
-private val MainActivity.mainViewModel1: MainViewModel
-    get() = ViewModelProvider(this).get(MainViewModel::class.java)
 
 class MainActivity : AppCompatActivity() {
     private lateinit var dataBinding: ActivityMainBinding
-
-    // by lazy 延迟初始化
-    private val db: MyDatabase by lazy {
-        myLog("创建 MyDatabase 对象")
-
-        Room.databaseBuilder(
-            this,
-            MyDatabase::class.java,
-            "mydata.db"
-        )
-            // Schema 有变化时，自动删除老的数据库，重建新的数据库
-            .fallbackToDestructiveMigration()
-            .build()
-    }
-
-    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,55 +15,17 @@ class MainActivity : AppCompatActivity() {
 
         dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-
-        val myViewModelFactory = MyViewModelFactory(application)
-        mainViewModel = ViewModelProvider(this, myViewModelFactory).get(MainViewModel::class.java)
-        mainViewModel.myDataList.observe(this) {
-            dataBinding.tvInfo.text = it.toString()
-        }
-
         with(dataBinding) {
+            refreshLayout.setOnRefreshListener {
+                tvInfo.text = "正在刷新"
+                btnRefresh.isEnabled = true
+            }
+
             btnRefresh.setOnClickListener {
-                // useList()
-                // useLiveData()
-                // collectFlow()
-                useRepository()
+                tvInfo.text = "下拉刷新"
+                refreshLayout.isRefreshing = false
+                btnRefresh.isEnabled = false
             }
         }
-    }
-
-    private fun useRepository() {
-        mainViewModel.addNewData()
-        showToast(this, "插入数据完成")
-    }
-
-    private fun useList() {
-        lifecycleScope.launch(Dispatchers.IO) {
-            val all = db.myDataDao().getAll()
-            runOnUiThread {
-                dataBinding.tvInfo.text = all.toString()
-            }
-        }
-    }
-
-    private fun useLiveData() {
-        val all = db.myDataDao().getAllReturnLiveData()
-        all.observe(this) {
-            dataBinding.tvInfo.text = it.toString()
-        }
-    }
-
-    private fun collectFlow() {
-        lifecycleScope.launch {
-            val all = db.myDataDao().getAllReturnFlow()
-            all.collect {
-                dataBinding.tvInfo.text = it.toString()
-            }
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        db.close()
     }
 }
